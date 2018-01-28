4db14e8663b61c89bb0c4801ef8c6d220b8ecf13
bmv-2143 <bmv-2143@yandex.ru>
2018-01-28 13:24:28 +0300

    Initial commit. Project version v0.001.
    First commit after migration from BitBucket to GitHub.

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/RocketLanderAcademy.cs
@@ -0,0 +1,31 @@
+﻿public class RocketLanderAcademy : Academy {
+
+    public float boardScaleX = 16;
+    public float boardScaleY = 10;
+
+    public float rocketStartPosBorderX;
+    public float rocketStartPosBorderY;
+
+    public override void InitializeAcademy()
+    {
+        UpdateCurriculumLearningParams();
+    }
+
+    public override void AcademyReset()
+	{
+        UpdateCurriculumLearningParams();
+    }
+
+    private void UpdateCurriculumLearningParams()
+    {
+        boardScaleX = (float)resetParameters["board_scale_x"];
+        boardScaleY = (float)resetParameters["board_scale_y"];
+
+        rocketStartPosBorderX = (float)resetParameters["rocket_start_pos_border_x"];
+        rocketStartPosBorderY = (float)resetParameters["rocket_start_pos_border_y"];
+    }
+
+ //   public override void AcademyStep()
+	//{
+	//}
+}
