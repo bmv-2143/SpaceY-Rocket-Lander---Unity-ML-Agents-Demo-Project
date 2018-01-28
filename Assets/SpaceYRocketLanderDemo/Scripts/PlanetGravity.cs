82084ddec0e291fc3718ebd742b643b1876c4ecc
bmv-2143 <bmv-2143@yandex.ru>
2018-01-28 13:24:28 +0300

    Initial commit. Project version v0.01.
    First commit after migration from BitBucket to GitHub.

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/PlanetGravity.cs
@@ -0,0 +1,41 @@
+﻿using HumbleGames.BallThrowerGame.Utilities;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour {
+
+    public Rigidbody gravityAffectedObject;
+    public float gravityRange;
+    public float gravityStrength = 25;
+
+	private void Start () {
+        DrawCircle drawCircleScript = GetComponent<DrawCircle>();
+        drawCircleScript.radiusXY = gravityRange;
+	}
+	
+    private void FixedUpdate()
+    {
+        ApplyPlanetGravity();
+    }
+
+    private void ApplyPlanetGravity()
+    {
+        if (IsInGravityRange(gravityAffectedObject))
+        {
+            Vector3 movementDirection = transform.position - gravityAffectedObject.transform.position;
+
+            Vector3 movementDirectionNormalized =
+                     new Vector3(movementDirection.x, movementDirection.y, 0).normalized;
+
+            Vector3 planetGravityForce = movementDirectionNormalized * gravityStrength;
+
+            gravityAffectedObject.AddForce(planetGravityForce, ForceMode.Acceleration);
+        }
+    }
+
+    private bool IsInGravityRange(Rigidbody objectForGravity)
+    {
+        return Vector3.Distance(
+                transform.position, 
+                objectForGravity.transform.position) <= gravityRange;
+    }
+}
---
+++ Assets/SpaceYRocketLanderDemo/Scripts/ProbeMonitor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class ProbeMonitor : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject leftLegLandingProbe;
+
+    [SerializeField]
+    private GameObject rightLegLandingProbe;
+
+    private bool leftLegLanded = false;
+    private bool rightLegLanded = false;
+    private bool accidentOccured = false;
+
+    public delegate void ProbeCollidedAction(Probe.ProbeType probeType, Probe.TriggerType collisionType);
+    public static event ProbeCollidedAction OnProbeCollided;
+
+    // ---------------------------------- Public API ----------------------------------------------
+
+    public static void SendProbeTriggerEvent(Probe.ProbeType probeType, Probe.TriggerType collisionType)
+    {
+        OnProbeCollided?.Invoke(probeType, collisionType);
+    }
+
+    public bool IsOnPlanet()
+    {
+        return leftLegLanded && rightLegLanded;
+    }
+
+    public bool AccidentOccured()
+    {
+        return accidentOccured;
+    }
+
+    public void Reset()
+    {
+        leftLegLanded = false;
+        rightLegLanded = false;
+        accidentOccured = false;
+    }
+
+    // ---------------------------------- Unity hook methods --------------------------------------
+
+    private void OnEnable()
+    {
+        OnProbeCollided += ProbeTriggerHandler;
+    }
+
+    private void OnDisable()
+    {
+        OnProbeCollided -= ProbeTriggerHandler;
+    }
+
+    // ----------------------------------- Private helpers ----------------------------------------
+
+    private void ProbeTriggerHandler(Probe.ProbeType probeType, Probe.TriggerType triggerType)
+    {
+        if (probeType == Probe.ProbeType.LEFT_LEG && triggerType == Probe.TriggerType.TRIGGER_STAY)
+        {
+            leftLegLanded = true;
+        }
+
+        if (probeType == Probe.ProbeType.RIGHT_LEG && triggerType == Probe.TriggerType.TRIGGER_STAY)
+        {
+            rightLegLanded = true;
+        }
+
+        if (probeType == Probe.ProbeType.HULL && triggerType == Probe.TriggerType.TRIGGER_ENTER)
+        {
+            accidentOccured = true;
+        }
+    }
+}
