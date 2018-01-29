985fd43ba284aa23231ea5c3ec43699d71cdbf62
bmv-2143 <bmv-2143@yandex.ru>
2018-01-29 16:26:29 +0300

    Quit app on Escape key

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/UserInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
