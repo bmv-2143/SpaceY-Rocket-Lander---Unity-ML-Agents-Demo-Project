21bde9d969e7c97c5493cc8667470d8024b7e8db
bmv-2143 <bmv-2143@yandex.ru>
2018-01-29 17:17:40 +0300

    Add editor menu Tools/SpaceY/DeletePlayerPrefs

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/Editor/SpaceYMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SpaceYMenu : EditorWindow
+{
+
+    [MenuItem("Tools/SpaceY/Reset Playerprefs")]
+
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
