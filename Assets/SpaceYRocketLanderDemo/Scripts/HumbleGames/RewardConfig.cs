de01c6c92b5749506dac030556c03bb75ff4bd75
bmv-2143 <bmv-2143@yandex.ru>
2020-08-05 19:49:30 +0300

    Add RewardConfig scriptable object

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/RewardConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HumbleGames
+{
+    [CreateAssetMenu(fileName = "RewardConfig", menuName = "ScriptableObjects/RewardConfig", order = 1)]
+    public class RewardConfig : ScriptableObject
+    {
+        public float landingSuccessReward = 1f;
+        public float deathZoneCollisionReward = -1f;
+        public float planetCollisionReward = -1f;
+
+        /// <summary>
+        /// Used for calculation of stepwise reward of RocketAgent.
+        /// </summary>
+        public float agentStepwiseRewardBase = -1f;
+    }
+}
