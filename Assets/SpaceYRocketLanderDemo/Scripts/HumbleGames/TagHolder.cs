30134d3eaeaf6e5f2c983102f07506ed46963bd1
bmv-2143 <bmv-2143@yandex.ru>
2020-08-05 19:47:49 +0300

    Add TagHolder scriptable object

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/TagHolder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HumbleGames
+{
+    [CreateAssetMenu(fileName = "TagHolder", menuName = "ScriptableObjects/TagHolder", order = 1)]
+    public class TagHolder : ScriptableObject
+    {
+        public string deathZone = "DeathZone";
+    }
+}
