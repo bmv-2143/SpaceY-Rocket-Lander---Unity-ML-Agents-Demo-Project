69a20a4b0ae0bb63597f4d16b093112b88946679
bmv-2143 <bmv-2143@yandex.ru>
2020-08-05 19:48:05 +0300

    Add SimulationState sctiptable object

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/SimulationState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace HumbleGames
+{
+    [CreateAssetMenu(fileName = "SimulationState", menuName = "ScriptableObjects/SimulationState", order = 1)]
+    public class SimulationState : ScriptableObject
+    {
+        public bool isDeathZoneCollisionAccident;
+        
+        public bool isPlanetCollisionAccident;
+
+        public bool isLeftLegLanded;
+
+        public bool isRightLegLanded;
+
+        public bool IsLandingSucces()
+        {
+            return isLeftLegLanded && isRightLegLanded;
+        }
+
+        public void Reset()
+        {
+            isDeathZoneCollisionAccident = false;
+            isPlanetCollisionAccident = false;
+            isLeftLegLanded = false;
+            isRightLegLanded = false;
+        }
+    }
+}
