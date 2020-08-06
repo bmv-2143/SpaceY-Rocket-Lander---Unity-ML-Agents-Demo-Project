e77f90108fcbdba46a0b33c5fe1e9320a5f90d90
bmv-2143 <bmv-2143@yandex.ru>
2020-08-06 16:24:21 +0300

    Reorganize objects in scene, add ObjectActiveStateController

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/ObjectsActiveStateController.cs
@@ -0,0 +1,28 @@
+﻿using Unity.MLAgents.Policies;
+using UnityEngine;
+
+namespace HumbleGames
+{
+    public class ObjectsActiveStateController : MonoBehaviour
+    {
+        [SerializeField]
+        private BehaviorParameters behaviorParameters;
+
+        [SerializeField]
+        private GameObject[] objsDeactivateInTrainingMode;
+
+        private void OnEnable()
+        {
+            // if it is not Heuristic mode => deactivate these objects for other modes (training)
+            foreach (GameObject go in objsDeactivateInTrainingMode)
+            {
+                go.SetActive(ShouldObjectBeActive());
+            }
+        }
+
+        private bool ShouldObjectBeActive()
+        {
+            return behaviorParameters.BehaviorType == BehaviorType.HeuristicOnly;
+        }
+    }
+}
