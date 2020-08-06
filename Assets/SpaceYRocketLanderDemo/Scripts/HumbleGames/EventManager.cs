d3c2f08541cb963500893c0d12040920a77295ef
bmv-2143 <bmv-2143@yandex.ru>
2020-08-06 14:02:02 +0300

    Add EventManager script

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/EventManager.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HumbleGames
+{
+    public class EventManager
+    {
+        public delegate void SimulationEndAction(SimulationFinishStatus status);
+        public static event SimulationEndAction OnSimulationEnd;
+
+        public static void RaiseSimulationEndEvent(SimulationFinishStatus status)
+        {
+            OnSimulationEnd?.Invoke(status);
+        }
+    }
+
+}
