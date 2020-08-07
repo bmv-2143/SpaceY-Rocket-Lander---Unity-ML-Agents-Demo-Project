da1407649263fa92436f21e726ae91abb90b8a6c
bmv-2143 <bmv-2143@yandex.ru>
2020-08-07 13:15:11 +0300

    Track success of last N simulations in SimulationTracker

--- Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/SimulationStatsTracker.cs
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/SimulationStatsTracker.cs
@@ -1,6 +1,7 @@
 ﻿using HumbleGames;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace HumbleGames
@@ -9,14 +10,23 @@ namespace HumbleGames
     {
         private readonly string LOG_TAG = typeof(SimulationStatsTracker).Name;
 
+        [SerializeField]
+        private int numLastSimsToTrack = 100;
+       
+        private int[] resultsOfLastTrackedSims;
+        private int lastTrackedSimulationsIndex;
+
         private int totalSimulations;
-        private int successCount;
-        private int failCount;
-        private float successRate;
+        private int totalSuccessCount;
+        private int totalFailCount;
+        private float totalSuccessRate;
+
 
         private void OnEnable()
         {
             EventManager.OnSimulationEnd += OnSimulationEnd;
+
+            resultsOfLastTrackedSims = new int[numLastSimsToTrack];
         }
 
         private void OnDisable()
@@ -30,22 +40,54 @@ namespace HumbleGames
 
             if (status == SimulationEndStatus.SUCCESS)
             {
-                successCount++;
+                totalSuccessCount++;
             }
 
             else
             {
-                failCount++;
+                totalFailCount++;
             }
 
-            successRate = 100f * successCount / totalSimulations;
+            totalSuccessRate = 100f * totalSuccessCount / totalSimulations;
 
-            Debug.LogFormat("{0}: totalSimulations: {1}, succesRate: {2}%, successCount: {3}, failCount: {4}",
-                            LOG_TAG,
+            UpdateLastSimulationsSet(status);
+
+            Debug.LogFormat("{0}: totalSimulations: {1},  " +
+                            "successRateOver[{2}]: {3}% " +
+                            "totalSuccesRate: {4}%, " +
+                            "totalSuccessCount: {5}, " +
+                            "totalFailCount: {6}",
+                            LOG_TAG, 
                             totalSimulations,
-                            successRate,
-                            successCount,
-                            failCount);
+                            numLastSimsToTrack,
+                            GetSuccessRateOfLastSimulationsSet(),
+                            totalSuccessRate,
+                            totalSuccessCount,
+                            totalFailCount);
+        }
+
+        private float GetSuccessRateOfLastSimulationsSet()
+        {
+            return 100f * resultsOfLastTrackedSims.Sum() / numLastSimsToTrack;
+        }
+
+        private void UpdateLastSimulationsSet(SimulationEndStatus status)
+        {
+            if (status == SimulationEndStatus.SUCCESS)
+            {
+                resultsOfLastTrackedSims[lastTrackedSimulationsIndex] = 1;
+            }
+            else
+            {
+                resultsOfLastTrackedSims[lastTrackedSimulationsIndex] = 0;
+            }
+
+            lastTrackedSimulationsIndex++;
+
+            if (lastTrackedSimulationsIndex >= numLastSimsToTrack)
+            {
+                lastTrackedSimulationsIndex = 0;
+            }
         }
     }
 }
