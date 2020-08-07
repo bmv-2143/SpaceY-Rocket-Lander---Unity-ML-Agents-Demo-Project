3fbdcc7a75e445a2efa6f9befd5bae102e10356e
bmv-2143 <bmv-2143@yandex.ru>
2020-08-07 12:12:55 +0300

    Add ReadOnly attribute scripts

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Read Only attribute.
+/// Attribute is use only to mark ReadOnly properties.
+/// 
+/// source: https://www.patrykgalach.com/2020/01/20/readonly-attribute-in-unity-editor/
+/// </summary>
+namespace HumbleGames.Attributes
+{
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+    }
+}
