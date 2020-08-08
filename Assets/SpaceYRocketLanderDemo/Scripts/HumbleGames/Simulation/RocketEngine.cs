7d94f5f7411064838d8198e0db2ffc45103992ba
bmv-2143 <bmv-2143@yandex.ru>
2020-08-08 19:22:49 +0300

    Move RocketEngine to Simulation namespace\dir

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/Simulation/RocketEngine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace HumbleGames.Simulation
+{
+    public class RocketEngine : MonoBehaviour
+    {
+        ///// <summary>
+        ///// The direction along Y in the local space. Rotate the engine object accodingly to change the
+        ///// force application direction.
+        ///// </summary>
+        //private Vector3 engineForceDirection;
+
+        //private void Awake()
+        //{
+        //    engineForceDirection = Vector3.up;
+        //}
+
+        public void ApplyEngineForce(Rigidbody rocketRb, float forceValue)
+        {
+            // see Local Direction of Vector3
+            // https://answers.unity.com/questions/634157/local-direction-of-vector3.html
+
+            rocketRb.AddForceAtPosition(
+            -transform.up * forceValue,
+            transform.position,
+            ForceMode.Acceleration);
+        }
+    }
+}
