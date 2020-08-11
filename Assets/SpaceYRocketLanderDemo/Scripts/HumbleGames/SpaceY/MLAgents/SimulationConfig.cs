003b75f71c671fb5d16d941e8be8edb30466461a
bmv-2143 <bmv-2143@yandex.ru>
2020-08-11 15:26:26 +0300

    Add SimulationConfig SO script and asset

---
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/SpaceY/MLAgents/SimulationConfig.cs
@@ -0,0 +1,42 @@
+﻿using Unity.MLAgents.Policies;
+using UnityEngine;
+
+namespace HumbleGames.SpaceY.MLAgents
+{
+    [CreateAssetMenu(fileName = "SimulationConfig", menuName = "ScriptableObjects/SimulationConfig", order = 1)]
+    public class SimulationConfig : ScriptableObject
+    {
+        // Is expected to be changed in Editor in Edit Mode only
+        public SimulationMode simulationMode;
+
+        // Can be switched from the UI at runtime
+        private BehaviorType behaviourType;
+
+        public BehaviorType SimulationBehaviourType
+        {
+            get
+            {
+                return behaviourType;
+            }
+
+            set 
+            {
+                behaviourType = value;
+                EventManager.RaiseBehaviourTypeChangedEvent(behaviourType);
+            }
+        }
+
+        public enum SimulationMode
+        {
+            /// <summary>
+            /// A user plays the game or watches how ML-Agent plays.
+            /// </summary>
+            Demo,
+
+            /// <summary>
+            /// Training is performed in Unity Editor.
+            /// </summary>
+            Training
+        }
+    }
+}
