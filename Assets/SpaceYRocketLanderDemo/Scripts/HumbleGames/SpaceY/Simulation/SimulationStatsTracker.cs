c3d2f72102f85a951bbbc4d337e89c58d0b43beb
bmv-2143 <bmv-2143@yandex.ru>
2020-08-12 21:46:36 +0300

    Add large sets simulations tracking

--- Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/SpaceY/Simulation/SimulationStatsTracker.cs
+++ Assets/SpaceYRocketLanderDemo/Scripts/HumbleGames/SpaceY/Simulation/SimulationStatsTracker.cs
@@ -8,10 +8,15 @@ namespace HumbleGames.SpaceY.Simulation
         private readonly string LOG_TAG = typeof(SimulationStatsTracker).Name;
 
         [SerializeField]
-        private int numLastSimsToTrack = 100;
+        private int numLastSimsToTrackSmallSet = 100;
+        
+        [SerializeField]
+        private int numLastSimsToTrackLargeSet = 1000;
        
-        private int[] resultsOfLastTrackedSims;
-        private int lastTrackedSimulationsIndex;
+        private int[] resultsOfLastTrackedSimsSmallSet;
+        private int[] resultsOfLastTrackedSimsLargeSet;
+        private int lastTrackedSimsSmallSetIndex;
+        private int lastTrackedSimsLargeSetIndex;
 
         private int totalSimulations;
         private int totalSuccessCount;
@@ -23,7 +28,8 @@ namespace HumbleGames.SpaceY.Simulation
         {
             EventManager.OnSimulationEnd += OnSimulationEnd;
 
-            resultsOfLastTrackedSims = new int[numLastSimsToTrack];
+            resultsOfLastTrackedSimsSmallSet = new int[numLastSimsToTrackSmallSet];
+            resultsOfLastTrackedSimsLargeSet = new int[numLastSimsToTrackLargeSet];
         }
 
         private void OnDisable()
@@ -47,46 +53,76 @@ namespace HumbleGames.SpaceY.Simulation
 
             totalSuccessRate = 100f * totalSuccessCount / totalSimulations;
 
-            UpdateLastSimulationsSet(status);
+            UpdateLastSimulationSets(status);
 
-            if (totalSimulations % 100 == 0)
+            if (totalSimulations % numLastSimsToTrackSmallSet == 0)
             {
-                Debug.LogFormat("{0}: totalSimulations: {1},  " +
+                Debug.LogFormat("{0}: SMALL: totalSimulations: {1},  " +
                                 "successRateOver[{2}]: {3}% " +
                                 "totalSuccesRate: {4}%, " +
                                 "totalSuccessCount: {5}, " +
                                 "totalFailCount: {6}",
-                                LOG_TAG, 
+                                LOG_TAG,
                                 totalSimulations,
-                                numLastSimsToTrack,
-                                GetSuccessRateOfLastSimulationsSet(),
+                                numLastSimsToTrackSmallSet,
+                                GetSuccessRateOfLastSimsSmallSet(),
+                                totalSuccessRate,
+                                totalSuccessCount,
+                                totalFailCount);
+            }
+
+            if (totalSimulations % numLastSimsToTrackLargeSet == 0)
+            {
+                Debug.LogFormat("{0}: LARGE: totalSimulations: {1},  " +
+                                "successRateOver[{2}]: {3}% " +
+                                "totalSuccesRate: {4}%, " +
+                                "totalSuccessCount: {5}, " +
+                                "totalFailCount: {6}",
+                                LOG_TAG,
+                                totalSimulations,
+                                numLastSimsToTrackLargeSet,
+                                GetSuccessRateOfLastSimsLargeSet(),
                                 totalSuccessRate,
                                 totalSuccessCount,
                                 totalFailCount);
             }
         }
 
-        private float GetSuccessRateOfLastSimulationsSet()
+        private float GetSuccessRateOfLastSimsSmallSet()
         {
-            return 100f * resultsOfLastTrackedSims.Sum() / numLastSimsToTrack;
+            return 100f * resultsOfLastTrackedSimsSmallSet.Sum() / numLastSimsToTrackSmallSet;
         }
 
-        private void UpdateLastSimulationsSet(SimulationEndStatus status)
+        private float GetSuccessRateOfLastSimsLargeSet()
+        {
+            // TODO: refactor, don't sum large sets each time
+            return 100f * resultsOfLastTrackedSimsLargeSet.Sum() / numLastSimsToTrackLargeSet;
+        }
+
+        private void UpdateLastSimulationSets(SimulationEndStatus status)
         {
             if (status == SimulationEndStatus.SUCCESS)
             {
-                resultsOfLastTrackedSims[lastTrackedSimulationsIndex] = 1;
+                resultsOfLastTrackedSimsSmallSet[lastTrackedSimsSmallSetIndex] = 1;
+                resultsOfLastTrackedSimsLargeSet[lastTrackedSimsLargeSetIndex] = 1;
             }
             else
             {
-                resultsOfLastTrackedSims[lastTrackedSimulationsIndex] = 0;
+                resultsOfLastTrackedSimsSmallSet[lastTrackedSimsSmallSetIndex] = 0;
+                resultsOfLastTrackedSimsLargeSet[lastTrackedSimsLargeSetIndex] = 0;
             }
 
-            lastTrackedSimulationsIndex++;
+            lastTrackedSimsSmallSetIndex++;
+            lastTrackedSimsLargeSetIndex++;
 
-            if (lastTrackedSimulationsIndex >= numLastSimsToTrack)
+            if (lastTrackedSimsSmallSetIndex >= numLastSimsToTrackSmallSet)
             {
-                lastTrackedSimulationsIndex = 0;
+                lastTrackedSimsSmallSetIndex = 0;
+            }
+
+            if (lastTrackedSimsLargeSetIndex >= numLastSimsToTrackLargeSet)
+            {
+                lastTrackedSimsLargeSetIndex = 0;
             }
         }
     }
